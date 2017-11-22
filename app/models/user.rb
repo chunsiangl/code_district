2c6cab31b2498467df6a627e2716812c85ae20ad
chunsiangl <cslim_324@hotmail.com>
2017-11-22 17:06:18 +0800

    fix password validation error

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
 	validates :first_name, presence: true
     validates :last_name, presence: true 
-	validates :password, presence: true, length: { :in => 7..20 }
+	validates :password, presence: true, length: { :in => 7..20 }, on: :create
 	validates :email, uniqueness: {case_sensitive: false, message: "Error: An account with this email already exists."}
 	validates :email, presence: true, format: { with: (/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i),message: "Error: Invalid email format." }
 
